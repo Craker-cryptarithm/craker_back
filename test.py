c75e26e12c3f70cb8574211a1c63088ba0558ef8
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-09-21 13:25:15 +0900

    test

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 from back import problem_maker_multiplication, problem_maker_addition, problem_maker_subtraction, problem_maker_division
 
-'''
 problem_maker_addition(9, 5)
 print('\n\n')
 problem_maker_subtraction(9, 5)
 print('\n\n')
 problem_maker_multiplication(9, 5)
 print('\n\n')
-'''
 problem_maker_division(9, 5)
